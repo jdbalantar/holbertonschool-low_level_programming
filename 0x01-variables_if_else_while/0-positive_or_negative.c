be69aadb432178397e6959ca6d483b250c66965c
jdbalantar <jdbalantar@gmail.com>
2021-02-07 23:17:08 +0000

    Modify 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is positive %d", n);
+printf("%d is positive", n);
 }
 else if (n == 0)
 {
-printf("is zero %d", n);
+printf("%d is zero", n);
 }
 else
 {
-printf("is negative %d", n);
+printf("%d is negative", n);
 }
 return (0);
 }
