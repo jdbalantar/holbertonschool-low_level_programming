82595a48043aee922b6c20341adb6d5eae5e5810
jdbalantar <jdbalantar@gmail.com>
2021-02-08 07:05:02 +0000

    Update files

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print
+ *
+ *Return: int
+ */
+int main(void)
+{
+char alfa;
+for (alfa = 'a' ; alfa <= 'z' ; alfa++)
+{
+putchar(alfa);
+}
+putchar('\n');
+return (0);
+}
+
+
+
+
+
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print
+ *
+ *Return: int
+ */
+int main(void)
+{
+char alfa;
+for (alfa = 'z' ; alfa >= 'a' ; alfa--)
+{
+putchar(alfa);
+}
+putchar('\n');
+return (0);
+}
+
