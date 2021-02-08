d574084e6f9022298bcce31dd9f79559862bbca9
jdbalantar <jdbalantar@gmail.com>
2021-02-08 05:10:36 +0000

    Modify 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print
+ *
+ *Return: int
+ */
+int main(void)
+{
+char alfa;
+for (alfa = 'a' ; alfa <= 'z' ; alfa++)
+{
+putchar (alfa);
+putchar ('\n');
+}
+return (0);
+}
