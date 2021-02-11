873080cd1ec3d189674010c7413b33dcfbd5f6e8
jdbalantar <jdbalantar@gmail.com>
2021-02-11 00:53:30 +0000

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,25 @@
 #include "holberton.h"
-
 /**
  * print_sign - print sign for number
  * Return: int
  * @n: input character
  */
-
 int print_sign(int n)
 {
-
-        if (n > 0)
-        {
+	if (n > 0)
+	{
 		_putchar('+');
 		return (1);
-        }
-        else if (n == 0)
-        {
-                _putchar('0');
-                return (0);
-        }
-        else
-        {
-                _putchar('-');
-                return (-1);
-        }
-        _putchar('\n');
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('\n');
 }
