eb5ec1599cb50b69c82b7f9a659555467c87b37a
jdbalantar <jdbalantar@gmail.com>
2021-02-11 00:46:26 +0000

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
 int print_sign(int n)
 {
 
-        if (n < 0)
+        if (n > 0)
         {
 		_putchar('+');
 		return (1);
@@ -17,10 +17,10 @@ int print_sign(int n)
                 _putchar('0');
                 return (0);
         }
-        else if (n > 1)
+        else if (n < 1)
         {
                 _putchar('-');
-                return (1);
+                return (-1);
         }
         _putchar('\n');
 }
