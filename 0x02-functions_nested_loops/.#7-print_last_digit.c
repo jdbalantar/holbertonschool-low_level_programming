a7a4b2480734b740f3bb06750c22328b22578a6f
jdbalantar <jdbalantar@gmail.com>
2021-02-12 04:11:02 +0000

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/.#7-print_last_digit.c
+++
@@ -1,1 +0,0 @@
-vagrant@vagrant-ubuntu-trusty-64.2080:1613100720
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,5 +11,16 @@ int print_last_digit(int num)
 {
 	int x = (num % 10);
 
+	if (num >= 0)
+	{
+		_putchar(num + '0');
+		return (num);
+	}
+	else
+	{
+		num = -num;
+		_putchar(num + '0');
+		return (num);
+	}
 	return (x);
 }
