f95865a09c96fa5bafb4675cfcf367748b910389
jdbalantar <jdbalantar@gmail.com>
2021-02-14 18:18:17 +0000

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,31 +9,15 @@
 
 void print_to_98(int n)
 {
-	int c;
-
-	if (n <= 98)
+	while (n != 98)
 	{
-		for (c = n; c <= 98; c++)
-		{
-			printf("%d", c);
-			if (c != 98)
-			{
-				printf(", ");
-
-			}
-		}
-
+		printf("%d", n);
+		printf(",");
+		printf(" ");
+		if (n < 98)
+			n++;
+		else
+			n--;
 	}
-	else
-	{
-		for (c = n; c >= 0; c--)
-		{
-			printf("%d", c);
-			if (c != 98)
-			{
-				printf(", ");
-			}
-		}
-	}
-	printf("\n");
+	printf("98\n");
 }
