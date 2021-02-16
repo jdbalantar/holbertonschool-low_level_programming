2b146cd1f8c735a098f6d90bf14e7751136f581b
jdbalantar <jdbalantar@gmail.com>
2021-02-16 04:18:24 +0000

    Update 6-lines.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: external input
+ * Return: linex
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n != 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar ('_');
+		}
+		_putchar ('\n');
+	}
+}
