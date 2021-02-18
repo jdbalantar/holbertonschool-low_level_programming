6c145c700125737307a72495dc34644087e1c758
jdbalantar <jdbalantar@gmail.com>
2021-02-18 23:25:43 +0000

    Update task 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a string, in reverse
+ * @s: external input
+ * Return: int
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar (s[j]);
+	}
+	_putchar(10);
+}
