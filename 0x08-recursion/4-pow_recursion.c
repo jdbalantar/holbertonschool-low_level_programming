05b7b89a4e43ff5a02b6db3af055cb7267566b1e
jdbalantar <jdbalantar@gmail.com>
2021-03-06 17:39:32 +0000

    Pow task

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: Base
+ * @y: Power
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * (_pow_recursion(x, y - 1)));
+}
