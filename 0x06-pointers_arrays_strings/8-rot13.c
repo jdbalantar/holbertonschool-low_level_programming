e436e10a28896fbf01ac807655537d6c31e845b4
jdbalantar <jdbalantar@gmail.com>
2021-03-06 01:45:23 +0000

    function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * *rot13 - Write a function that encodes a string using rot13.
+ * @s:external input
+ * Return:0
+ **/
+char *rot13(char *s)
+{
+	int x = 0, y;
+
+	char alpha[] = {
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+		'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+		'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+		'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+	char r13[] = {
+		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+		'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a',
+		'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
+	while (s[x])
+	{
+		for (y = 0; y < 52; y++)
+		{
+			if (s[x] == alpha[y])
+			{
+				s[x] = r13[y];
+				break;
+			}
+		}
+		x++;
+	}
+	return (s);
+}
